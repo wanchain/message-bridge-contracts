c88016617fe7df1882d34b7876089751a34e1d53
lolieatapple <lolieatapple@gmail.com>
2023-09-05 18:01:13 +0800

    add deployment

--- README.md
+++ README.md
@@ -10,6 +10,20 @@ The contract code mainly consists of two parts: one part is WmbGateway, and the 
 
 The WMB Gateway is a smart contract that acts as an intermediary between the Wanchain Message Bridge and the external blockchain networks. It provides a secure and efficient mechanism for transferring messages between the different networks. 
 
+### Deployed SC address
+
+The current test network is using upgradeable contracts for convenient functionality upgrades and maintenance. The main network will use non-upgradeable contracts to ensure security.
+
+| Network | Contract Address |
+| --- | --- |
+| Wanchain Testnet | [0xEB14407Edc497a73934dE08D5c3079BB1F5f145D](https://testnet.wanscan.org/address/0xEB14407Edc497a73934dE08D5c3079BB1F5f145D) |
+| Ethereum Goerli Testnet  | [0x9454C2F15F308098163623D5E7deCe366793efD3](https://goerli.etherscan.io/address/0x9454C2F15F308098163623D5E7deCe366793efD3) |
+| Avalanche Fuji Testnet | [0x8Ee72C8194ec8A527B1D4981742727437091C913](https://testnet.snowtrace.io/address/0x8Ee72C8194ec8A527B1D4981742727437091C913) |
+| XDC testnet | [0x8c1b9daD87BFC48DF48b15baA19d0FB163030169](https://apothem.xinfinscan.com/address/xdc8c1b9daD87BFC48DF48b15baA19d0FB163030169#transactions) |
+| Arbitrum Goerli Testnet | [0x294B79d3D13DAb36C51C8E4Cf3c2Cd3948F0bA4C](https://testnet.arbiscan.io/address/0x294B79d3D13DAb36C51C8E4Cf3c2Cd3948F0bA4C) |
+| Optimism Goerli Testnet | [0x9B9492466F70e0dA0f4ef0aC27a53550B0769232](https://goerli-optimism.etherscan.io/address/0x9B9492466F70e0dA0f4ef0aC27a53550B0769232) |
+| Polygon Mumbai Testnet | [0x61F4aBa60A158E180521264a793fCb2901fCe998](https://mumbai.polygonscan.com/address/0x61F4aBa60A158E180521264a793fCb2901fCe998) |
+
 ## WMB App
 
 WMB App is a smart contract that can be inherited by third-party DApps to interact with the WMB Gateway. It provides a customizable interface for interacting with the WMB Gateway and handling incoming messages.
--- scripts/deploy_testnet.js
+++ scripts/deploy_testnet.js
@@ -10,22 +10,28 @@ const fs = require('fs');
 // admin address 
 const ADMIN = '0xF6eB3CB4b187d3201AfBF96A38e62367325b29F9';
 // current chain bip44 chainId
-const bip44_chainId = "2147484198";
+const bip44_chainId = "2147484614";
 
 // contract address for MPC signature verification
 const SIGNATURE_VERIFIER = {
-  '2153201998': '0x5dcab781bd5e1e7af64eec0686f6d618554f6340', // wanchain 
-  '2147483708': '0x08bad1a48b0b08bf769f83ba30c1dad0f8bb8b6b', // ethereum 
-  '2147492648': '0x0a5b5ea60930cca901bce3e3ad1772ebdd5065b8', // Avalanche
+  '2153201998': '0x5dcab781bd5e1e7af64eec0686f6d618554f6340', // wanchain testnet
+  '2147483708': '0x08bad1a48b0b08bf769f83ba30c1dad0f8bb8b6b', // ethereum testnet
+  '2147492648': '0x0a5b5ea60930cca901bce3e3ad1772ebdd5065b8', // Avalanche testnet
   '2147484198': '0x35b90f99680c426bf6753a78c364b045115cb46e', // XDC testnet
+  '1073741826': '0x6e81e127a280cd17bebc7704ce28a0226295a12b', // arb testnet
+  '2147484262': '0x35b90f99680c426bf6753a78c364b045115cb46e', // opt testnet
+  '2147484614': '0x4c12e6696fe23b15f2b911db7ca42b2d01cde84a', //polygon mumbai
 };
 // contract address for get smg info, oracle proxy on other evm chain or smgAdmin on wanchain
 const SMG_PROXY = {
-  '2153201998': '0xaA5A0f7F99FA841F410aafD97E8C435c75c22821', // wanchain
-  '2147483708': '0x0f0bf93bf16fd28294c637d855fc73b917ef5fcc', // ethereum
-  '2147492648': '0x302554d20c92461f4c57bad481797b6d5f422c45', // Avalanche
-  '2147484198': '0x9c843263bd9ae7277ab7c29c18e3f532894a0d35', // XDC testnet
-}; 
+  '2153201998': '0xaA5A0f7F99FA841F410aafD97E8C435c75c22821', // wanchain testnet
+  '2147483708': '0x0f0bf93bf16fd28294c637d855fc73b917ef5fcc', // ethereum testnet
+  '2147492648': '0x302554d20c92461f4c57bad481797b6d5f422c45', // Avalanche testnet
+  '2147484198': '0x9c843263bd9ae7277ab7c29c18e3f532894a0d35', // XDC testnet 
+  '1073741826': '0x35b90f99680c426bf6753a78c364b045115cb46e', // arb testnet 
+  '2147484262': '0x9b281146a04a67948f4601abda704016296017c5', // opt testnet
+  '2147484614': '0xdff6a8699031a448c4cc130f2bfdd9d2db4e5877', // polygon mumbai
+};  
 
 // fee calculation
 const feeInfo = {
@@ -34,8 +40,8 @@ const feeInfo = {
     gasPrice: 2e9,
   },
   '2147483708': { // ethereum
-    coinPriceUsd: 1814,
-    gasPrice: 1e9,
+    coinPriceUsd: 1622,
+    gasPrice: 0.02e9,
   },
   '2147492648': { // Avalanche
     coinPriceUsd: 16.98,
@@ -44,16 +50,23 @@ const feeInfo = {
   '2147484198': {
     coinPriceUsd: 0.033,
     gasPrice: 1e9,
-  }
+  },
+  '1073741826': {
+    coinPriceUsd: 1622,
+    gasPrice: 0.1e9,
+  },
+  '2147484262': {
+    coinPriceUsd: 1622,
+    gasPrice: 0.01e9,
+  },
+  '2147484614': {
+    coinPriceUsd: 0.555,
+    gasPrice: 2e9,
+  },
 }
 
 // supported dst chains and base fee
-let supporedChainIds = [
-  '2147483708', // ethereum
-  '2153201998', // wanchain
-  '2147492648', // Avalanche
-  '2147484198', // XDC testnet
-];
+let supporedChainIds = Object.keys(SMG_PROXY);
 
 let supportedDstChains = {};
 supporedChainIds.map((chainId) => {
@@ -61,6 +74,8 @@ supporedChainIds.map((chainId) => {
     baseFee: (feeInfo[chainId].coinPriceUsd * feeInfo[chainId].gasPrice / feeInfo[bip44_chainId].coinPriceUsd).toFixed(0),
   };
 });
+
+console.log('supportedDstChains', supportedDstChains);
 
 // remove current chain from supportedDstChains
 delete supportedDstChains[bip44_chainId];
@@ -130,7 +145,7 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
