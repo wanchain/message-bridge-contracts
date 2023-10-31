4bab70012c9dc5a95adbdc24962203ee18e77aa9
lolieatapple <lolieatapple@gmail.com>
2023-10-31 14:07:30 +0800

    add proxy

---
+++ contracts/utils/WmbGatewayProxy.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+/**
+ * Wanchain Message Bridge
+ * https://wanchain.org/ 
+ */
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract WmbGatewayProxy is TransparentUpgradeableProxy {
+    constructor(
+        address _logic,
+        address admin_,
+        bytes memory _data
+    ) payable TransparentUpgradeableProxy(_logic, admin_, _data) {}
+}
