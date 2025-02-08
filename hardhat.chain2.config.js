c6ddd8cafa0c1342dda2a2ca731ed5ab09c2edea
lolieatapple <lolieatapple@gmail.com>
2025-02-08 10:39:35 +0800

    fix sandbox compile errors

--- hardhat.chain2.config.js
+++ hardhat.chain2.config.js
@@ -2,7 +2,29 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+        }
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+          evmVersion: "london"
+        }
+      }
+    ]
+  },
   networks: {
     hardhat: {
       bip44_chainId: 2,
