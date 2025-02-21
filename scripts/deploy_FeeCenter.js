1422787c860c14e2fbb9db0984e32e1a4bc14eb1
lolieatapple <lolieatapple@gmail.com>
2025-02-21 12:06:10 +0800

    chore: update deploy script

--- scripts/deploy_FeeCenter.js
+++ scripts/deploy_FeeCenter.js
@@ -1,6 +1,6 @@
 const hre = require("hardhat");
 
-const UPGRADE_ADMIN_ADDRESS = "0x5560aF0F46D00FCeA88627a9DF7A4798b1b10961";
+const UPGRADE_ADMIN_ADDRESS = "0xff72713c834a96f3c2f28145b4f05e5faf39e572";
 
 async function main() {
   let deployer = (await hre.ethers.getSigner()).address;
